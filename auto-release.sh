1610e21806960fa480784c4509022b5235072285
André Seibt <andre.seibt@talk-point.de>
2023-02-17 15:41:55 +0100

    git flow auto command

---
+++ auto-release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+version=$(git tag --sort version:refname | tail -n 1)
+
+major=$(echo $version | cut -d. -f1)
+minor=$(echo $version | cut -d. -f2)
+patch=$(echo $version | cut -d. -f3)
+patch=$((${patch} + 1))
+
+version="${major}.${minor}.${patch}"
+git flow release start "${version}"
